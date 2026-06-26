c861a13a119f5afa84bd9068d3c6d2cedc4a95ba
RAFAELESPADA <pidgeypidgey1@gmail.com>
2026-06-25 22:22:27 -0300

    First323222212´~22222n222~k222222

--- src/com/rafaelauler/bedwars/ArenaBlockBreakListener.java
+++ src/com/rafaelauler/bedwars/ArenaBlockBreakListener.java
@@ -1,67 +1,42 @@
 package com.rafaelauler.bedwars;
 
-
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-public class ArenaBlockBreakListener
-        implements Listener {
+public class ArenaBlockBreakListener implements Listener {
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onBreak(
-	        BlockBreakEvent e) {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBreak(BlockBreakEvent e) {
 
-	    GamePlayer gp =
-	            Bedwars.getInstance()
-	                    .getPlayerManager()
-	                    .get(
-	                            e.getPlayer()
-	                    );
+        GamePlayer gp = Bedwars.getInstance()
+                .getPlayerManager()
+                .get(e.getPlayer());
 
-	    if(gp == null)
-	        return;
+        if (gp == null || gp.getArena() == null)
+            return;
 
-	    Arena arena =
-	            gp.getArena();
+        Arena arena = gp.getArena();
 
-	    if(arena == null)
-	        return;
+        // Só permite quebrar blocos durante a partida
+        if (arena.getState() != ArenaState.PLAYING) {
+            e.setCancelled(true);
+            return;
+        }
 
-	    if(e.getBlock().getType()
-	            == Material.BED_BLOCK)
-	        return;
+        // A cama é tratada pelo BedWarsListeners
+        if (e.getBlock().getType() == Material.BED_BLOCK)
+            return;
 
-	    Location loc =
-	            e.getBlock()
-	                    .getLocation();
+        // Se foi um bloco colocado durante a partida, pode quebrar
+        if (arena.getPlacedBlocks().remove(e.getBlock().getLocation())) {
+            e.setCancelled(false);
+            return;
+        }
 
-	    Location found = null;
-	    for(Location placed : arena.getPlacedBlocks()) {
-
-	        if(placed.getWorld().equals(loc.getWorld())
-	                && placed.getBlockX() == loc.getBlockX()
-	                && placed.getBlockY() == loc.getBlockY()
-	                && placed.getBlockZ() == loc.getBlockZ()) {
-
-	            found = placed;
-	            break;
-	        }
-	    }
-
-	    if(found != null) {
-
-	        arena.getPlacedBlocks().remove(found);
-
-	        e.setCancelled(false);
-
-	        return;
-	    }
-
-	    e.setCancelled(true);
-	}
-	
+        // Caso contrário é um bloco do mapa
+        e.setCancelled(true);
+    }
 }
