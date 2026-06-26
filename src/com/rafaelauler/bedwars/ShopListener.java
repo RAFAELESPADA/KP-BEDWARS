09fd582451c1b014d2337fc708af98d6618fee9a
RAFAELESPADA <pidgeypidgey1@gmail.com>
2026-06-25 23:17:03 -0300

    First323222212´~22222n222~k22222222ç2222

--- src/com/rafaelauler/bedwars/ShopListener.java
+++ src/com/rafaelauler/bedwars/ShopListener.java
@@ -320,8 +320,7 @@ public class ShopListener implements Listener {
             player.updateInventory();
             return;
         }
-        if(item.getSwordTier()
-                != null && !item.getDisplay().getType().name().contains("_SWORD") && !item.getDisplay().getType().name().contains("_AXE")) {
+        if(item.getDisplay().getType().name().contains("_SWORD") && !item.getDisplay().getType().name().contains("_AXE")) {
         player.getInventory()
         .addItem(
                 item.getReward()
